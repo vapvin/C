933649824e6dafcd10bc863ad523358a097e01bb
vapvin <tpehdj12@daum.net>
2020-01-01 12:47:59 +0900

    Umm...

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ll long long
 
 typedef struct {
     int data;
@@ -13,4 +14,50 @@ Node* initNode(int data, Node* leftChild, Node* rightChild) {
     node->leftChild = leftChild;
     node->rightChild = rightChild;
     return node;
+}
+
+// 전위 순회
+
+void preorder(Node* root){
+    if(root){
+        printf("%d", root->data);
+        preorder(root->leftChild);
+        preorder(root->rightChild);
+    }
+}
+
+// 중위 순회
+
+void inorder(Node* root) {
+    if(root){
+        inorder(root->leftChild);
+        printf("%d", root->data);
+        inorder(root->rightChild);
+    }
+}
+
+// 후위 순회
+
+void postorder(Node* root) {
+    if(root){
+        postorder(root->leftChild);
+        postorder(root->rightChild);
+        printf("%d", root->data);
+    }
+}
+
+int main(void) {
+    Node* n7 = initNode(50, NULL, NULL);
+    Node* n6 = initNode(37, NULL, NULL);
+    Node* n5 = initNode(23, NULL, NULL);
+    Node* n4 = initNode(5, NULL, NULL);
+    Node* n3 = initNode(48, n6, n7);
+    Node* n2 = initNode(17, n4, n5);
+    Node* n1 = initNode(30, n2, n3);
+    preorder(n1);
+    printf("\n");
+    inorder(n1);
+    printf("\n");
+    postorder(n1);
+    return 0;
 }
