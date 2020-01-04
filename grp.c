3016dbaf54a642234b253f5d389cc22a24603809
vapvin <tpehdj12@daum.net>
2020-01-04 18:00:42 +0900

    Graph

---
+++ grp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int a[1001][1001];
+int m,n;
+
+int main(void) {
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < m; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        a[x][y] = 1;
+        a[y][x] = 1;
+    }
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            printf("%d", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
