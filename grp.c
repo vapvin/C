27b4dc169d5c0e9520ddc45bca826cee261d4514
vapvin <tpehdj12@daum.net>
2020-01-04 18:30:48 +0900

    Def

--- grp.c
+++ grp.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int a[1001][1001];
-int m,n;
+typedef struct {
+    int index;
+    int distance;
+    struct Node *next;
+} Node;
 
-int main(void) {
-    scanf("%d %d", &n, &m);
-    for(int i = 0; i < m; i++) {
-        int x, y;
-        scanf("%d %d", &x, &y);
-        a[x][y] = 1;
-        a[y][x] = 1;
+void addFront(Node *root, int index, int distance) {
+    Node *node = (Node*)malloc(sizeof(Node));
+    node->index = index;
+    node->distance = distance;
+    node->next = root->next;
+    root->next = node;
+}
+
+void showAll(Node *root){
+    Node *cur = root->next;
+    while(cur != NULL) {
+        printf("%d(거리:%d)", cur->index, cur->distance);
+        cur = cur->next;
     }
+}
 
+int main(void){
+    int n, m;
+    scanf("%d %d", &n, &m);
+    Node** a = (Node*)malloc(sizeof(Node*) * (n + 1));
     for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            printf("%d", a[i][j]);
-        }
+        a[i] = (Node*)malloc(sizeof(Node));
+        a[i]->next = NULL;
+    }
+    for(int i = 0; i < m; i++) {
+        int x, y, distance;
+        scanf("%d %d %d", &x, &y, &distance);
+        addFront(a[x], y, distance);
+    }
+    for(int i = 1; i <= n; i++){
+        printf("원소 [%d]: ", i);
+        showAll(a[i]);
         printf("\n");
     }
 
