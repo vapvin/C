51fe9b8c2ebff641223f7f0e82344c0be12fdb02
vapvin <tpehdj12@daum.net>
2020-01-05 20:34:15 +0900

    AVL Tree Rotate LL and RR

--- avl.c
+++ avl.c
@@ -13,3 +13,34 @@ typedef struct {
     struct Node* rightChild;
 } Node;
 
+int getHight(Node* node) {
+    if(node == NULL) return 0;
+    return node->height;
+}
+
+void setHeight(Node* node) {
+    node->height = getMax(getHeight(node->leftChild), getHeight(node->rightChild)) + 1;
+}
+
+int getDifference(Node* node) {
+    if(node == NULL) return 0;
+    Node* leftChild = node->leftChild;
+    Node* rightChild = node->rightChild;
+    return getHeight(leftChild) - getHeight(rightChild);
+}
+
+Node* rotateLL(Node* node) {
+    Node *leftChild = node->leftChild;
+    node->leftChild = leftChild->rightChild;
+    leftChild->rightChild = node;
+    setHeight(node);
+    return leftChild;
+}
+
+Node* rotateRR(Node* node) {
+    Node *rightChild = node->rightChild;
+    node->rightChild = rightChild->leftChild;
+    rightChild->leftChild = node;
+    setHeight(node);
+    return rightChild;
+}
