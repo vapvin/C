1d8d3db66946c8837796f965dba6ef77bb604bba
vapvin <tpehdj12@daum.net>
2020-01-06 18:46:30 +0900

    Func Push

--- prime.c
+++ prime.c
@@ -10,6 +10,12 @@ typedef struct
     int node;
 } Edge;
 
+typedef struct
+{
+    Edge *heap[EDGE_MAX];
+    int count;
+} priorityQueue;
+
 void swap(Edge *a, Edge *b)
 {
     Edge temp;
@@ -19,4 +25,20 @@ void swap(Edge *a, Edge *b)
     a->node = b->node;
     b->cost = temp.cost;
     b->node = temp.node;
+}
+
+void push(priorityQueue *pq, Edge *edge)
+{
+    if (pq->ciunt >= EDGE_MAX)
+        return;
+    pq->heap[pq->count] = edge;
+    int now = pq->count;
+    int parent = (pq->count - 1) / 2;
+    while (now > 0 && pq->heap[now]->cost < pq->heap[parent]->cost)
+    {
+        swap(pq->heap[now], pq->heap[parent]);
+        now = parent;
+        parent = (parent - 1) / 2;
+    }
+    pq->count++;
 }
