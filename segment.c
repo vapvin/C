69fb5d8ae3cec38f6dfa10470d48336d26b0136b
vapvin <tpehdj12@daum.net>
2020-01-08 12:31:13 +0900

    Sum Func

--- segment.c
+++ segment.c
@@ -13,3 +13,17 @@ int init(int start, int end, int node)
     int mid = (start + end) / 2;
     return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
 }
+
+int sum(int start, int end, int node, int left, int right)
+{
+    if (left > end || right < start)
+    {
+        return 0;
+    }
+    if (left <= start && end <= right)
+    {
+        return tree[node];
+    }
+    int mid = (start + end) / 2;
+    return sum(start, mid, node * 2, left, right) + (mid + 1, end, node * 2 + 1, left, right);
+}
