ca3065001dfba0937b1b68c5451fa794dc12e528
vapvin <tpehdj12@daum.net>
2020-01-08 12:34:34 +0900

    Update Func

--- segment.c
+++ segment.c
@@ -27,3 +27,20 @@ int sum(int start, int end, int node, int left, int right)
     int mid = (start + end) / 2;
     return sum(start, mid, node * 2, left, right) + (mid + 1, end, node * 2 + 1, left, right);
 }
+
+void update(int start, int end, int node, int index, int dif)
+{
+    if (index < start || index > end)
+    {
+        return;
+    }
+    tree[node] += dif;
+    if (start == end)
+    {
+        return;
+    }
+
+    int mid = (start + end) / 2;
+    update(start, mid, node * 2, index, dif);
+    update(mid + 1, end, node * 2 + 1, index, dif);
+}
