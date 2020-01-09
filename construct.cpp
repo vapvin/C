41bd7266f968681fd9f08cdaf47a3c5d8508cfdf
vapvin <tpehdj12@daum.net>
2020-01-09 18:35:55 +0900

    Make Class and Constructor

---
+++ construct.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Charactor {
+    private:
+        string name;
+        int ragePoint;
+        int hp;
+        int damage;
+    public:
+        Charactor(string name, int hp, int damage){
+            this->name = name;
+            this->ragePoint = 0;
+            this->hp = hp;
+            this->damage = damage;
+        }
+};
