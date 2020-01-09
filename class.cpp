96d57235e939354b7ff04a8d0e3a1aa0d03a1889
vapvin <tpehdj12@daum.net>
2020-01-09 15:18:47 +0900

    C++ Class

---
+++ class.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int score;
+public:
+    Student(string n, int s) {name = n; score=s;};
+
+};
