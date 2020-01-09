e92e55d46bd987fac0f66af092fa9c397a3faece
vapvin <tpehdj12@daum.net>
2020-01-09 15:21:15 +0900

    Using Class

--- class.cpp
+++ class.cpp
@@ -8,6 +8,13 @@ private:
     string name;
     int score;
 public:
-    Student(string n, int s) {name = n; score=s;};
-
+    Student(string n, int s) {name = n; score=s;}
+    void show() { cout << name << ":" << score << "점\n";}
 };
+
+int main(void) {
+    Student a = Student("Vins", 100);
+    a.show();
+
+    return 0;
+}
