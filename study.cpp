a7ddbc250387f1a1edf73b51ee3bea0b6b92203d
vapvin <tpehdj12@daum.net>
2020-03-22 17:24:27 +0900

    Inheritance 02

--- study.cpp
+++ study.cpp
@@ -19,3 +19,23 @@ public:
         cout << "Name :" << getName() << "\n";
     }
 };
+
+class Dob: Dog{
+private:
+    int birthday;
+public:
+    Dob(int birthday, string name, int legs): Dog(name, legs){
+        this->birthdate = birthday;
+    }
+    void show(){
+        cout << "Birthday" << birthday << "\n";
+        cout << "DogName" << getName() << "\n";
+        cout << "Dog Legs" << getLegs() << "\n";
+    }
+};
+
+int main(void) {
+    Dob dob(0712, "lin");
+    dob.show();
+    system("pause");
+}
