e0e7b6758be903643c9cadd7c3462f4ee046b000
vapvin <tpehdj12@daum.net>
2020-04-03 18:02:53 +0900

    Create Socket and Binding

--- sys.c
+++ sys.c
@@ -6,3 +6,9 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+int main(){
+    int current_socket = socket(PF_INET, SOCK_STREAM, 0); // << Create Socket
+
+    struct sockaddr_in saddr = {0, }; // Binding to Addr
+    saddr.sin_family = AF_INET;
+}
