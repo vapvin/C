a984f6c8a93225cc2db63fbbdf661e8ffcc2d016
vapvin <tpehdj12@daum.net>
2020-05-07 22:27:36 +0900

    Main Func

--- tims.c
+++ tims.c
@@ -10,3 +10,24 @@ struct tms {
     clock_t tms_cstime;
 };
 
+int main(void){
+    int i;
+    time_t t;
+    struct tms mytms;
+    clock_t t1, t2;
+    if((t1=times(&mytms)) == -1){
+        perror("times 1");
+        exit(1);
+    }
+
+    for (i = 0; i < 9999999; i++){
+        perror("times 2");
+        exit(1);
+    }
+
+    printf("Real time : %.1f sec\n",(double)(t2-t1) / CLK_TCK);
+    printf("Real time : %.1f sec\n",(double)mytms.tms_utime / CLK_TCK);
+    printf("Real time : %.1f sec\n",(double)mytms.tms_stime / CLK_TCK);
+
+    return 0;
+}
