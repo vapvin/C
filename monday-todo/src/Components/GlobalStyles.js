c3f84ffea7e905ac90fb28ab5639929d62ac3fdf
vapvin <vins@vins.dev>
2020-07-15 22:57:22 +0900

    Test Files

---
+++ monday-todo/src/Components/GlobalStyles.js
@@ -0,0 +1,22 @@
+import {createGlobalStyle} from 'styled-components';
+import reset from 'styled-reset';
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+    * {
+        box-sizing: border-box;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    body {
+        font-family:--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 12px;
+        background: #eee;
+        color: #222;
+        padding-top: 60px;
+    }
+`;
+
+export default globalStyles;
--- monday-todo/src/Components/Header.js
+++ monday-todo/src/Components/Header.js
@@ -3,19 +3,35 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Header = styled.header`
-
+    display: flex;
+    align-items: center;
+    color: #f2f2f2;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    background: rgba(10,10,10,0.8);
+    z-index: 10;
+    box-shadow: 0 1px 5px 2px rgba(0,0,0,0.8);
 `;
 
 const List = styled.ul`
     display: flex;
-    &:hover {
-        background: #000;
-    }
 `; 
 
-const Item = styled.li``;
+const Item = styled.li`
+    width: 80px;
+    height: 50px;
+    text-align: center;
+`;
 
-const SLink = styled(Link)``;
+const SLink = styled(Link)`
+    display: flex;
+    height: 50px;
+    align-items: center;
+    justify-content: center;
+`;
 
 export default () => {
     return(
