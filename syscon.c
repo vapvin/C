0e6be2b9062ddc5c4e8755687ff27092f994a373
vapvin <vins@vins.dev>
2020-07-21 22:28:45 +0900

    Copy

--- syscon.c
+++ syscon.c
@@ -19,3 +19,21 @@ int main(void){
 
     return 0;
 }
+
+int main(void){
+    char buf[257];
+
+    if (sysinfo(SI_HW_SERIAL, buf, 257) == -1) {
+        perror("sysinfo");
+        exit(1);
+    }
+    printf("HW Serial : %s\n", buf);
+
+    if(sysinfo(SI_ISALIST, buf, 257) == -1){
+        perror("sysinfo");
+        exit(1);
+    }
+    printf("ISA List : %s\n", buf);
+
+    return 0;
+}
