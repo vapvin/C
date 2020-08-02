9bb11c24dc3120b85f003a359fd235286b4e1c84
vapvin <tpehdj12@daum.net>
2020-08-02 17:46:46 +0900

    Umm Testing this files

--- scrape.py
+++ scrape.py
@@ -1,3 +1,21 @@
-from urllib2 from urlopen
+from urllib2 import urlopen
 import time
 
+def scrape(url, term):
+
+    response = urlopen(url)
+
+    html = response.read()
+
+    ts = time.time()
+
+    filename = "scrape" + str(ts) + ".html"
+
+    f = open(filename, "w")
+
+    if term in s.read():
+        return "Fount" + term + "in" + url
+
+    return term + "was not found in" + url
+
+scraoe(url, term)
