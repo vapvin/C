b8747828253c397b4185854e7fd4cd1af9253e43
vapvin <tpehdj12@daum.net>
2020-08-10 16:19:08 +0900

    Definition Color and Usage() func

---
+++ nmap.sh
@@ -0,0 +1,14 @@
+# namp automator code..
+
+# Color Definitions
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+SECONDS= 0
+
+usage(){
+    
+}
--- rust-scrap/scrap.rs
+++ rust-scrap/scrap.rs
@@ -10,3 +10,7 @@ fn write_file(filename: &str, data: &str){
     file.write_all(bytes).expect("Error Writing File");
 }
 
+fn read_file(filename: &str) -> String{
+    let mut file = File::open(filename).expect("Error File Open");
+    let mut contents = String::new();
+}
