831d4f11dd5074e477a4c0f20daaffaccdad15cd
vapvin <tpehdj12@daum.net>
2020-08-10 16:22:16 +0900

    Clear this Func

--- nmap.sh
+++ nmap.sh
@@ -10,5 +10,18 @@ NC='\033[0m'
 SECONDS= 0
 
 usage(){
-    
+    echo -e ""
+    echo -e "${RED}Usage: $0 <TARGET-IP> <TYPE>"
+    echo -e "${YELLOW}"
+    echo -e "Scan Types:"
+    echo -e "\tQuick:	Shows all open ports quickly (~15 seconds)"
+    echo -e "\tBasic:	Runs Quick Scan, then runs a more thorough scan on found ports (~5 minutes)"
+    echo -e "\tUDP:	Runs \"Basic\" on UDP ports (~5 minutes)"
+    echo -e "\tFull:	Runs a full range port scan, then runs a thorough scan on new ports (~5-10 minutes)"
+    echo -e "\tVulns:	Runs CVE scan and nmap Vulns scan on all found ports (~5-15 minutes)"
+    echo -e "\tRecon:	Suggests recon commands, then prompts to automatically run them"
+    echo -e "\tAll:	Runs all the scans (~20-30 minutes)"
+    echo -e ""
+    exit 1
 }
+
